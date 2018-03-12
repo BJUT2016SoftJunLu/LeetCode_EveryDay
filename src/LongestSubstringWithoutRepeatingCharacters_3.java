77df908aa2695d7a5a21134f1aba52065f894aa2
dingjunlu <dingjunlu@corp.netease.com>
2018-03-12 11:48:45 +0800

    test

--- src/LongestSubstringWithoutRepeatingCharacters_3.java
+++ src/LongestSubstringWithoutRepeatingCharacters_3.java
@@ -27,45 +27,4 @@ public class LongestSubstringWithoutRepeatingCharacters_3 {
         }
         return maxSub;
     }
-
-
-    /*
-      Sliding Window
-    * Time complexity : O(n)
-    * Space complexity : O(n)
-    * */
-    public static Integer maxSubSize(String str,int length){
-
-        Integer maxSize = 0;
-        HashSet<Character> set = new HashSet<Character>();
-        int i = 0,j = 0;
-        while(i < length && j<length){
-            if(!set.contains(str.charAt(j))){
-                set.add(str.charAt(j++));
-                maxSize = Math.max(maxSize,j - i);
-            }else{
-                set.remove(str.charAt(i++));
-            }
-        }
-        return maxSize;
-    }
-
-
-    public static void main(String []args){
-        String str = "jkklmmds";
-        int length = str.length();
-        System.out.println(LongestSubstringWithoutRepeatingCharacters_3.maxSubString(str,length));
-        System.out.println(LongestSubstringWithoutRepeatingCharacters_3.maxSubSize(str,length));
-
-
-        String indexName = "test-日期-维度-期刊 ";
-        String []name_dimension = Arrays.copyOfRange(indexName.split("-"),1,indexName.split("-").length);
-
-        Arrays.sort(name_dimension);
-
-        for(String name:name_dimension){
-            System.out.print(name);
-        }
-    }
-
 }
