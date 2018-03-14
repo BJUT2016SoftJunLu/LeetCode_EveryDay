1e22db9fef90405f8c16d9c5c7dd21848ed9128d
dinglu <1102800233@qq.com>
2018-03-14 19:00:17 +0800

    test

--- src/ReverseInteger_7.java
+++ src/ReverseInteger_7.java
@@ -1,18 +1,17 @@
-
-
 /*
 *
 * Input: 123
   Output:  321
 * */
 
-public class ReverseInteger {
+public class ReverseInteger_7 {
 
 
     public static Integer reverseInteger(int x){
 
         int a = 0,b = x;
         while(b > 0){
+            //先求摸，后乘10
             a = a * 10 + b % 10;
             b = b / 10;
         }
@@ -20,7 +19,7 @@ public class ReverseInteger {
     }
 
     public static void main(String []args){
-        System.out.println(ReverseInteger.reverseInteger(123));
+        System.out.println(ReverseInteger_7.reverseInteger(123));
     }
 
 }
